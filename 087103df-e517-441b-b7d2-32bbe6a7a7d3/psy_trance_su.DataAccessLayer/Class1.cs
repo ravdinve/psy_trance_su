d3d16ab88696203970736c7a7d14ed6a9194f6e8
Вячеслав Равдин <ravdinve@yandex.ru>
2017-07-07 01:47:04 +0300

    07.07.2017 1:47

---
+++ 087103df-e517-441b-b7d2-32bbe6a7a7d3/psy_trance_su.DataAccessLayer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psy_trance_su.DataAccessLayer
+{
+    public class Class1
+    {
+    }
+}
